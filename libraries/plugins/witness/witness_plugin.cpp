44eb03bf74ceb34d937cbfe007e34bb13649db36
lopu <ireply@myleisure.com.au>
2018-07-23 20:32:58 +1000

    ready

--- libraries/plugins/witness/witness_plugin.cpp
+++ libraries/plugins/witness/witness_plugin.cpp
@@ -62,7 +62,7 @@ void new_chain_banner( const steemit::chain::database& db )
       "********************************\n"
       "*                              *\n"
       "*   ------- NEW CHAIN ------   *\n"
-      "*   -   Welcome to Steem!  -   *\n"
+      "*   -   Welcome to Ezira!  -   *\n"
       "*   ------------------------   *\n"
       "*                              *\n"
       "********************************\n"
