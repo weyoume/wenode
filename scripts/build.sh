dab6109dacb84c6cd44441f814e3808c4d5e9cac
lopu <ireply@myleisure.com.au>
2018-07-24 23:13:18 +1000

    ready

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,13 +1,3 @@
-# export origindir=$(pwd)
-# rm -fr ../build-dir ;
-# mkdir -p ../build-dir ;
-# cp -r ./* ../build-dir/ ;
-# rm -fr ../build-dir/.git ;
-# mkdir -p ../build-dir/.git ;
-# cp -r $(awk '{ print $2 }' .git)/* ../build-dir/.git ;
-# cd ../build-dir ;
-git submodule update --init --recursive ;
-docker build . ;
-# cd $origindir ;
+docker build -t eziranetwork/ezira . ;
 
 
---
+++ scripts/run-full-peer-witness-miner-api.sh
@@ -0,0 +1,5 @@
+docker run \
+	--env USE_WAY_TOO_MUCH_RAM=1 \ 
+	--env USE_FULL_WEB_NODE=1 \
+	-d -p 2001:2001 -p 8090:8090 --name ezira \
+	eziranetwork/ezira
