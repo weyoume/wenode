6db8ab8c29522ee9a4701a5cd7f1eb4ca50197a1
lopu <lopu@lopudesigns.com>
2018-07-24 13:14:41 +0000

    ready

--- scripts/dockerfull-loadbalanced.sh
+++ scripts/dockerfull-loadbalanced.sh
@@ -1,6 +1,3 @@
-docker run \
-	--env USE_WAY_TOO_MUCH_RAM=1 \ 
-	--env USE_FULL_WEB_NODE=1 \
-	--env USE_MULTICORE_READONLY=1 \
-	-d -p 2001:2001 -p 8090:8090 -p 8091:8091 --name ezira-lb \
+docker run --env USE_WAY_TOO_MUCH_RAM=1 --env USE_FULL_WEB_NODE=1 --env USE_MULTICORE_READONLY=1 \
+	-d -p 11111:11111 -p 12345:12345 --name ezira-lb \
 	eziranetwork/ezira-lb
