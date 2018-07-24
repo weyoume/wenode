9b6476286d74fb9c1b98b14fcf92bf5dd6405600
lopu <ireply@myleisure.com.au>
2018-07-25 02:23:40 +1000

    ready

--- scripts/run-full-peer-witness-miner-api-cluster-X.sh
+++ scripts/run-full-peer-witness-miner-api-cluster-X.sh
@@ -7,6 +7,7 @@ export peerCount=${1:-${#witnesses[@]}}
 keys="$(cat ./contrib/credentials/witnesses.txt | awk -F' ' '{print $2}')"
 keys=($keys)
 nodescount=0
+docker network create eznodes
 for ((i=0;i<$peerCount;i++)) ; do
 	if [[ ! -z ${witnesses[$i]} ]] ; then
 		echo "#### LOOP "$i" ####"
@@ -17,7 +18,7 @@ for ((i=0;i<$peerCount;i++)) ; do
 		echo "full web node"
 		echo "peer, witness, api"
 
-		docker run --ip $subnet$(($i+3)) --env USE_WAY_TOO_MUCH_RAM=1 --env USE_FULL_WEB_NODE=1 --env STEEMD_WITNESS_NAME=${witnesses[$i]} --env STEEMD_PRIVATE_KEY=${keys[$i]}  -d --name ezira$i eziranetwork/ezira & 
+		docker run --ip $subnet$(($i+3)) --network eznodes --env USE_WAY_TOO_MUCH_RAM=1 --env USE_FULL_WEB_NODE=1 --env STEEMD_WITNESS_NAME=${witnesses[$i]} --env STEEMD_PRIVATE_KEY=${keys[$i]}  -d --name ezira$i eziranetwork/ezira & 
 		((nodescount++))
 		# sleep 1
 	fi
