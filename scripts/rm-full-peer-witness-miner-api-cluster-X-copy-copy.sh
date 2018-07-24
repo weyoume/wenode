3140d0d0f07ac8a5a901cd84b3f5d39d119e55d2
lopu <ireply@myleisure.com.au>
2018-07-25 00:41:02 +1000

    ready

--- scripts/rm-full-peer-witness-miner-api-cluster-X-copy-copy.sh
+++ scripts/rm-full-peer-witness-miner-api-cluster-X-copy-copy.sh
@@ -10,6 +10,8 @@ for ((i=0;i<${#witnesses[@]};i++))
 		echo "LOOP"
 		echo $i
 		echo "starting docker container ezira"$i" with witness "${witnesses[$i]}
-		docker run --env USE_WAY_TOO_MUCH_RAM=1 --env USE_FULL_WEB_NODE=1 --env STEEMD_WITNESS_NAME=${witnesses[$i]} --env STEEMD_PRIVATE_KEY=${keys[$i]} --env STEEMD_MINER_NAME=${witnesses[$i]} -d --name ezira$i eziranetwork/ezira
+		# docker run --env USE_WAY_TOO_MUCH_RAM=1 --env USE_FULL_WEB_NODE=1 --env STEEMD_WITNESS_NAME=${witnesses[$i]} --env STEEMD_PRIVATE_KEY=${keys[$i]} --env STEEMD_MINER_NAME=${witnesses[$i]} -d --name ezira$i eziranetwork/ezira
+		# docker stop ezira$i
+		docker rm ezira$i
 		sleep 1
 done
