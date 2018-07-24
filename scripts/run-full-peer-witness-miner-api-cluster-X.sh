b2d6cb40df06f1c8cf6c358380a962d1a13d2f15
lopu <ireply@myleisure.com.au>
2018-07-25 00:50:35 +1000

    ready

--- scripts/run-full-peer-witness-miner-api-cluster-X.sh
+++ scripts/run-full-peer-witness-miner-api-cluster-X.sh
@@ -9,8 +9,14 @@ for ((i=0;i<${#witnesses[@]};i++))
 	do
 		echo "LOOP #"$i
 		echo "starting docker container ezira"$i" with witness "${witnesses[$i]}
+		echo "no linked ports, nginx-router will handle"
+		echo "image eziranetwork/ezira"
+		echo "not load balanced internally"
+		echo "full web node"
+		echo "peer, witness, miner, api"
+
 		docker run --env USE_WAY_TOO_MUCH_RAM=1 --env USE_FULL_WEB_NODE=1 --env STEEMD_WITNESS_NAME=${witnesses[$i]} --env STEEMD_PRIVATE_KEY=${keys[$i]} --env STEEMD_MINER_NAME=${witnesses[$i]} -d --name ezira$i eziranetwork/ezira
-		sleep 1
+		# sleep 1
 done
 
 echo "ALL DONE :D you started "${#witnesses[@]}" nodes"
