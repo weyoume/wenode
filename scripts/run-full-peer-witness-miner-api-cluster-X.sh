0721513c90dc270ef48ac3ec1c44469aeb9c1734
lopu <ireply@myleisure.com.au>
2018-07-25 01:43:42 +1000

    ready

--- scripts/run-full-peer-witness-miner-api-cluster-X.sh
+++ scripts/run-full-peer-witness-miner-api-cluster-X.sh
@@ -7,20 +7,20 @@ export peerCount=${1:-${#witnesses[@]}}
 keys="$(cat ./contrib/credentials/witnesses.txt | awk -F' ' '{print $2}')"
 keys=($keys)
 nodescount=0
-for ((i=0;i<$peerCount;i++))
-	do
-		if [[ ! -z ${witnesses[$i]} ]]; then
-			echo "#### LOOP "$1" ####"
-			echo "starting docker container ezira"$i" with witness "${witnesses[$i]}
-			echo "no linked ports, nginx-router will handle"
-			echo "image eziranetwork/ezira"
-			echo "not load balanced internally"
-			echo "full web node"
-			echo "peer, witness, api"
+for ((i=0;i<$peerCount;i++)) ; do
+	if [[ ! -z ${witnesses[$i]} ]] ; then
+		echo "#### LOOP "$1" ####"
+		echo "starting docker container ezira"$i" with witness "${witnesses[$i]}
+		echo "no linked ports, nginx-router will handle"
+		echo "image eziranetwork/ezira"
+		echo "not load balanced internally"
+		echo "full web node"
+		echo "peer, witness, api"
 
-			docker run --env USE_WAY_TOO_MUCH_RAM=1 --env USE_FULL_WEB_NODE=1 --env STEEMD_WITNESS_NAME=${witnesses[$i]} --env STEEMD_PRIVATE_KEY=${keys[$i]}  -d --name ezira$i eziranetwork/ezira & 
-			(($nodescount++))
-			# sleep 1
+		docker run --env USE_WAY_TOO_MUCH_RAM=1 --env USE_FULL_WEB_NODE=1 --env STEEMD_WITNESS_NAME=${witnesses[$i]} --env STEEMD_PRIVATE_KEY=${keys[$i]}  -d --name ezira$i eziranetwork/ezira & 
+		(($nodescount++))
+		# sleep 1
+	fi
 done
 
 echo "ALL DONE :D you started "$nodescount" nodes out of "$peerCount" desired"
