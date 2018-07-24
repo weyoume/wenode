4bdee9e8f2a925ee1747cb0b7fad587377a025ff
lopu <lopu@lopudesigns.com>
2018-07-24 13:14:41 +0000

    ready

--- scripts/dockerfull.sh
+++ scripts/dockerfull.sh
@@ -1,6 +1,3 @@
-docker run \
-	--env USE_WAY_TOO_MUCH_RAM=1 \ 
-	--env USE_FULL_WEB_NODE=1 \
-	--env USE_MULTICORE_READONLY=1 \
-	-d -p 2001:2001 -p 8090:8090 -p 8091:8091 --name ezira \
-	eziranetwork/ezira
+docker run --env USE_WAY_TOO_MUCH_RAM=1 --env USE_FULL_WEB_NODE=1 \
+	-d -p 2001:2001 -p 8090:8090 -p 80:8090 -p 8080:8090  --name ezira0 \
+	-td ezira4
