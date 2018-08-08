3cbaa88ddc97942590251a2a8b835ef45fce61b0
lopu <ireply@myleisure.com.au>
2018-08-08 19:41:03 +1000

    ready

--- scripts/build_native.sh
+++ scripts/build_native.sh
@@ -2,15 +2,15 @@
 # an absolute path to the directory your github 
 # repo for the ezira node software resides in
 
-cd $repos/ezira-node/dev ;
+# cd $repos/ezira-node/dev ;
 rm -r build/ ;
 mkdir build && cd build ;
 cmake \
 	-DCMAKE_BUILD_TYPE=Release \
+	# -DCMAKE_INSTALL_PREFIX=$repos/ezira-node/dev/made \
 	-DLOW_MEMORY_NODE=OFF \
 	-DCLEAR_VOTES=OFF \
 	-DSKIP_BY_TX_ID=ON \
-	-DCMAKE_INSTALL_PREFIX=$repos/ezira-node/dev/made \
 	..
 
 # make -j$(sysctl -n hw.logicalcpu)
