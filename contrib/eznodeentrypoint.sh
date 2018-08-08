5ec97c7a14af0379c748d53778440d870a6ff5d0
lopu <ireply@myleisure.com.au>
2018-08-08 20:05:38 +1000

    ready

--- contrib/eznodeentrypoint.sh
+++ contrib/eznodeentrypoint.sh
@@ -6,7 +6,7 @@ ulimit -c unlimited
 # if we're not using PaaS mode then start eznode traditionally with sv to control it
 if [[ ! "$USE_PAAS" ]]; then
   mkdir -p /etc/service/eznode
-  cp /usr/local/bin/ezira-sv-run.sh /etc/service/eznode/run
+  cp /usr/local/bin/eznode-sv-run.sh /etc/service/eznode/run
   chmod +x /etc/service/eznode/run
   runsv /etc/service/eznode
 else
