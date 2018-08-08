750906cf357af55337e1bf32851ce9d9817ee24f
lopu <ireply@myleisure.com.au>
2018-08-08 20:45:06 +1000

    ready

--- scripts/run-full-peer-witness-miner-api-multi-witness-template.sh
+++ scripts/run-full-peer-witness-miner-api-multi-witness-template.sh
@@ -5,18 +5,17 @@ offset=1
 witness_names=""
 witness_base_name="ezbuilder"
 for ((n=0;n<$num;n++)); do
-	echo $n
 	witness_names+="$witness_base_name$((n+offset)) "
 done
 
-echo $witness_names
+echo [$witness_names]
 
-docker run \
-	--env USE_WAY_TOO_MUCH_RAM=1 \
-        --env USE_FULL_WEB_NODE=1 \
-        --env EZNODE_PRIVATE_KEY="5JbDVBTnNJh2WCPjHqEpSBKgnmm6cVE3tGNTW4HQZ9kYzDnAM11" \
-        --env EZNODE_WITNESS_NAMES=$witness_names \
-       	--env EZNODE_WITNESS_NAME="ezbuilder" \
-        -d -p 2001:2001 -p 8090:8090 --name ezira-test-node \
-        lopudesigns/ezira-test-node
+# docker run \
+# 	--env USE_WAY_TOO_MUCH_RAM=1 \
+#         --env USE_FULL_WEB_NODE=1 \
+#         --env EZNODE_PRIVATE_KEY="5JbDVBTnNJh2WCPjHqEpSBKgnmm6cVE3tGNTW4HQZ9kYzDnAM11" \
+#         --env EZNODE_WITNESS_NAMES=$witness_names \
+#        	--env EZNODE_WITNESS_NAME="ezbuilder" \
+#         -d -p 2001:2001 -p 8090:8090 --name ezira-test-node \
+#         lopudesigns/ezira-test-node
 
