d709906f1a78286a7aac267c632a5c4910d1740a
lopu <ireply@myleisure.com.au>
2018-08-10 02:46:16 +1000

    ready

--- scripts/run-full-peer-witness-miner-api-multi-witness-template.sh
+++ scripts/run-full-peer-witness-miner-api-multi-witness-template.sh
@@ -4,6 +4,7 @@ docker rm -f ezira-test-node ;
 
 
 # will make ezbuilder1 - ezbuilder99 if end=100 and start=0
+
 start=0
 end=100
 witness_names=""
@@ -18,7 +19,7 @@ docker run \
 	--env USE_WAY_TOO_MUCH_RAM=1 \
         --env USE_FULL_WEB_NODE=1 \
 				--env USE_NGINX_FRONTEND=1 \
-        --env PRIVATE_KEY="5JbDVBTnNJh2WCPjHqEpSBKgnmm6cVE3tGNTW4HQZ9kYzDnAM11" \
+        --env PRIVATE_KEY=$PRIVATE_KEY \
         --env WITNESS_NAMES="$witness_names" \
        	--env WITNESS_NAME="ezbuilder" \
         -d -p 2001:2001 -p 8090:8090 --name ezira-test-node \
